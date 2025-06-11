5de86e2d69d5ab01f14559719a0b50ee18f0685a
Prakyat_Shetty <prakyatshetty5@gmail.com>
2025-06-11 14:20:29 +0530

    Aboutaicss me changes

--- src/components/AboutAi.jsx
+++ src/components/AboutAi.jsx
@@ -13,6 +13,7 @@ const AboutAi = () => {
             frameBorder='0' 
             width='100%' 
             height='100%'
+            allow="fullscreen"
           />
          
           {/* Content Section */}
