6758e5be55b108e4e88ca5e6890e15010c925be4
Bhavith Shetty <bhavithshetty2203@gmail.com>
2025-06-17 13:38:25 +0530

    bhavithh

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,45 +1,34 @@
 import "./NavBar.css"
+import { useNavigate } from 'react-router-dom'
 
 export default function NavBar(){
-    const handleSmoothScroll = (e, targetId) => {
-    e.preventDefault()
-    const targetElement = document.getElementById(targetId)
-    if (targetElement) {
-      targetElement.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      })
-    }
-  }
-
+    const navigate = useNavigate()
     
-
+    function HandleClick(){
+        navigate('')
+    }
+    
     return (
+        <div className="fixed">
             <header>
-            <h1 className="logo">Askora</h1>
-            <nav>
-                <a href="#home" onClick={(e) => handleSmoothScroll(e, "home")}>
-                Home
-                </a>
-                <a href="#about-ai" onClick={(e) => handleSmoothScroll(e, "about-ai")}>
-                About AI
-                </a>
-                <a href="#Features" onClick={(e) => handleSmoothScroll(e, "Features")}>
-                Features
-                </a>
-                <a href="#pricing" onClick={(e) => handleSmoothScroll(e, "pricing")}>
-                Pricing
-                </a>
-                <a href="#community" onClick={(e) => handleSmoothScroll(e, "community")}>
-                Community
-                </a>
-            </nav>
-            <div className="buttons">
-            <button className="login boton-elegante">Login</button>
-            <button className="get-started cosmic ">Get Started &#8594;</button>
-            </div>
-        </header>
-
-        
+                <h1 className="logo">Askora</h1>
+                <nav>
+                    <a href="#">Home</a>
+                    <a href="#">About AI</a>
+                    <a href="#">Features</a>
+                    <a href="#">Pricing</a>
+                    <a href="#">Reviews</a>
+                </nav>
+                <div className="buttons">
+                    <button className="login boton-elegante">Login</button>
+                    <button 
+                        className="get-started cosmic" 
+                        onClick={HandleClick}
+                    >
+                        Get Started &#8594;
+                    </button>
+                </div>
+            </header>
+        </div>
     )
 }
